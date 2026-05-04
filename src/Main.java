fd251024b2cdd2ca86b9bbc9c28f0c24be9e88a7
bdeana <88990386+bdeana@users.noreply.github.com>
2026-05-04 18:58:12 +0200

    Dodan ispis za mjesec

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         LocalDateTime dt = LocalDateTime.now();
         System.out.println(dt + "Godina: " + dt.getYear() + ", Mjesec: " + dt.getMonth());
+        System.out.println("Mjesec: " + dt.getMonth());
     }
 }
